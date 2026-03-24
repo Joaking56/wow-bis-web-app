c757b043948b0ec25102ac110cf65025e258a724
joaking <okos100@gmail.com>
2026-03-24 01:45:18 +0100

    Folias Finish

--- pages/Folias.py
+++ pages/Folias.py
@@ -4,18 +4,18 @@ import bg_func
 
 bg_func.set_background("images/hunter.jpg", darkness=0.6)
 
-missing_items = functions.get_todos("texts/rudzi.txt")
-found_items = functions.get_todos("texts/completed_rudzi.txt")
+missing_items = functions.get_todos("texts/licsikee.txt")
+found_items = functions.get_todos("texts/completed_licsikee.txt")
 
 def save_edit():
     if "editing" in st.session_state and "edit_input" in st.session_state:
         new_value = st.session_state["edit_input"]
         index = found_items.index(st.session_state["editing"])
         found_items[index] = new_value + "\n"
-        functions.write_todos(found_items, "texts/completed_rudzi.txt")
+        functions.write_todos(found_items, "texts/completed_licsikee.txt")
         del st.session_state["editing"]
 
-st.title("Rudzi KPOP Demon Hunter BIS Checklist (Midnight Season 1)")
+st.title("The Grand Son Of Herkas BIS Checklist (Midnight Season 1)")
 
 col_input, col_add = st.columns([4, 1])
 with col_input:
@@ -27,7 +27,7 @@ with col_add:
     if st.button("Add"):
         if st.session_state["new_item"] != "":
             missing_items.append(st.session_state["new_item"] + "\n")
-            functions.write_todos(missing_items, "texts/rudzi.txt")
+            functions.write_todos(missing_items, "texts/licsikee.txt")
             del st.session_state["new_item"]
             st.rerun()
     st.markdown("</div>", unsafe_allow_html=True)
@@ -37,9 +37,9 @@ for index, missing_item in enumerate(missing_items):
     checkbox = st.checkbox(missing_item, key=f"missing_{index}")
     if checkbox:
         found_items.append(missing_item)
-        functions.write_todos(found_items, "texts/completed_rudzi.txt")
+        functions.write_todos(found_items, "texts/completed_licsikee.txt")
         missing_items.pop(index)
-        functions.write_todos(missing_items, "texts/rudzi.txt")
+        functions.write_todos(missing_items, "texts/licsikee.txt")
         del st.session_state[f"missing_{index}"]
         st.rerun()
 
@@ -53,7 +53,7 @@ with col1:
 with col2:
     if st.button("Delete", disabled=selected_item is None):
         found_items.remove(selected_item)
-        functions.write_todos(found_items, "texts/completed_rudzi.txt")
+        functions.write_todos(found_items, "texts/completed_licsikee.txt")
         st.rerun()
 
 if "editing" in st.session_state:
